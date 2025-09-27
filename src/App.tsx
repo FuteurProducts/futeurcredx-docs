78b6b493c669511b4b102b049558192465fddf2d
Shiv Vyas <shivvyas0209@gmail.com>
2025-09-27 14:20:54 -0400

    chore: trigger redeployment
    - Add comment to App.tsx to trigger Vercel redeployment
    - No functional changes to code

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// Main App Router Component
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
